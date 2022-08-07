bbfe64874d85491e8f1b23846b245db588249177
Suvankar Das <suvankar_das@outlook.com>
2022-08-07 13:06:00 +0530

    Add files via upload

---
+++ col2bw.py
@@ -0,0 +1,20 @@
+
+from PIL import Image
+# from tqdm import tqdm
+import os
+
+imgs = os.listdir(os.getcwd()+'/samples/')
+print("Total files in folder:", len(imgs))
+
+print("\nSelecet range to convert images:(1-30)")
+start = int(input("Enter starting index: "))
+end = int(input("Enter ending index: "))
+
+if 1 > start or len(imgs) < end: print(f"Error: Invalid indexing (Range: 1-{len(imgs)})")
+else:
+    # for i, j in enumerate(tqdm(imgs[start-1 : end])):
+        # Image.open(os.getcwd() + '/samples/' + imgs[i-1]).convert('L').save(os.getcwd() + '/converted/' + imgs[i-1][:imgs[i-1].find('.')] + '.png')
+    for i in range(len(imgs[start-1 : end])):
+        Image.open(os.getcwd() + '/samples/' + imgs[i-1]).convert('L').save(os.getcwd() + '/converted/' + imgs[i-1])
+    print("\n-> Result:"+ str(end - (start-1)) + " images converted successfully !\nHit ENTER to exit...", end='')
+    input()
