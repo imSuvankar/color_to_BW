27d0c707b691d80c7aae67955c0d3b29dd130b2b
Abhipsa Adhikary <70021736+rivieraviews@users.noreply.github.com>
2023-07-20 00:41:42 +0530

    Update col2bw.py
    Added alternative to a progress bar view of conversion process.

--- col2bw.py
+++ col2bw.py
@@ -12,6 +12,7 @@ end = int(input("Enter ending index: "))
 
 if 1 > start or len(imgs) < end: print(f"Error: Invalid indexing (Range: 1-{len(imgs)})")
 else:
+    # if you want a progress bar, uncomment lines 16 and 17, and comment out lines 18 and 19.
     # for i, j in enumerate(tqdm(imgs[start-1 : end])):
         # Image.open(os.getcwd() + '/samples/' + imgs[i-1]).convert('L').save(os.getcwd() + '/converted/' + imgs[i-1][:imgs[i-1].find('.')] + '.png')
     for i in range(len(imgs[start-1 : end])):
